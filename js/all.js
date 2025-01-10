90aa616335e1bb6210b17e30bcdb2489e91f28e2
Omar Abdullah <oe61031@gmail.com>
2025-01-10 14:06:13 +0200

    edit page

--- js/all.js
+++ js/all.js
@@ -1,17 +1,22 @@
 // varibls
-var logo = "<h1>logo</h>";
-var userImage = "./images/5830368875861098035.jpg";
-var userName = "omar abdullah";
-var userPosetion = "frontEnd & Founder, Example";
-var userLearning = "elazhar University";
-var userGithup = "https://github.com/El-Coach";
-var userGmail = "oe61031@gmail.com";
-var userFacebook = "https://facebook.com";
-var userWhatsapp = "https://wa.me/201091921248";
-
+const profile = {
+  logo: " logo",
+  userImage: "./images/5830368875861098035.jpg",
+  userName: "omar abdullah",
+  userPosetion: "frontEnd & Founder, Example",
+  userLearning: "elazhar University",
+  userGithup: "https://github.com/El-Coach",
+  userGmail: "oe61031@gmail.com",
+  userFacebook: "https://facebook.com",
+  userWhatsapp: "https://wa.me/201091921248",
+};
 // end vaibls
 
-// topFunction
+// add profile to local
+
+const ff = localStorage.setItem("profile", JSON.stringify(profile));
+
+// startScrollFunctions
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
@@ -21,17 +26,46 @@ function scrollWin() {
     window.scrollBy(0, -90);
   }, 500);
 }
+// endtScrollFunctions
 
-// stat-about
-document.getElementById("logo").innerHTML = logo;
-document.getElementById("userImage").src = userImage;
-document.getElementById("name").innerHTML = userName;
-document.getElementById("posetion").innerHTML = userPosetion;
-document.getElementById("learning").innerHTML = userLearning;
+// pullForm
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("edit-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const updatedProfile = {
+      logo: document.getElementById("editlogo").value,
+      userImage: "./images/5830368875861098035.jpg",
+      userName: document.getElementById("editUserName").value,
+      userPosetion: document.getElementById("editUserPosetion").value,
+      userLearning: document.getElementById("editUserLearning").value,
+      userGithup: document.getElementById("editUserGithup").value,
+      userGmail: document.getElementById("editUserGmail").value,
+      userFacebook: document.getElementById("editUserFacebook").value,
+      userWhatsapp: document.getElementById("editUserWhatsapp").value,
+    };
+    localStorage.setItem("profile", JSON.stringify(updatedProfile));
+    // window.location.href = "index.html";
+  });
+});
+
+// endPullForm
+
+const updatedProfile = JSON.parse(localStorage.getItem("profile"));
+console.log(updatedProfile);
+
+// start-about
+document.getElementById("logo").innerHTML = profile.logo;
+document.getElementById("userImage").src = profile.userImage;
+document.getElementById("name").innerHTML = profile.userName;
+document.getElementById("posetion").innerHTML = profile.userPosetion;
+document.getElementById("learning").innerHTML = profile.userLearning;
 // end-about
 // start-contact
-document.getElementById("facebook").href = userFacebook;
-document.getElementById("whatsapp").href = userWhatsapp;
-document.getElementById("gmail").href = userGmail;
-document.getElementById("github").href = userGithup;
+document.getElementById("facebook").href = profile.userFacebook;
+document.getElementById("whatsapp").href = profile.userWhatsapp;
+document.getElementById("gmail").href = profile.userGmail;
+document.getElementById("github").href = profile.userGithup;
 // end-contact
